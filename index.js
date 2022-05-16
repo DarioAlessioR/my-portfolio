606f297f220c057d694fd29e848d52237aa82cd8
Joffrey <joyjoffrey@gmail.com>
2022-05-17 00:47:44 +0200

    add events with javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+let ul = document.getElementById('show');
+let open = document.getElementById('open');
+let links = document.querySelectorAll('.menu-link ');
+
+if(ul.classList.contains('show')){
+  ul.classList.remove('show')
+}
+links.forEach(link=>{
+link.addEventListener('click', ()=>{
+  ul.classList.remove('show')
+})
+})
+
+open.addEventListener('click',()=>{
+ ul.classList.add('show')
+})
