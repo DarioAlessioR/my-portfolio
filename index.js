550defca420bbe961d6807d312696acb8cb82835
Joffrey <joyjoffrey@gmail.com>
2022-05-17 04:32:01 +0200

    update events functionalities and css styles on mobile menu

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let ul = document.getElementById('show');
-let open = document.getElementById('open');
-let links = document.querySelectorAll('.menu-link ');
+const ul = document.getElementById('show');
+const open = document.getElementById('open');
+const links = document.querySelectorAll('.menu-link');
 
-if(ul.classList.contains('show')){
-  ul.classList.remove('show')
+if (ul.classList.contains('show')){
+  ul.classList.remove('show');
 }
-links.forEach(link=>{
-link.addEventListener('click', ()=>{
-  ul.classList.remove('show')
-})
-})
 
-open.addEventListener('click',()=>{
- ul.classList.add('show')
- console.log("Hola")
-})
+links.forEach((link)=>{
+link.addEventListener('click', ()=>{
+  ul.classList.remove('show');
+});
+});
+
+open.addEventListener('click', () => {
+  ul.classList.add('show');
+});
